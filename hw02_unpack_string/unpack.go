f2762761874b8da8387b66e87df99d78d6df2983
Григорий Карман <g.karman@mail.ru>
2025-02-15 18:14:23 +0300

    Дом задание 2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,53 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(textForUnpack string) (string, error) {
+	var resultStringBuilder strings.Builder
+	var prevRune rune
+	length := len(textForUnpack)
+
+	for i, currentRune := range textForUnpack {
+		prevRuneIsDigit := unicode.IsDigit(prevRune)
+		currentRuneIsDigit := unicode.IsDigit(currentRune)
+
+		if prevRuneIsDigit && currentRuneIsDigit {
+			return "", ErrInvalidString
+		}
+
+		if !prevRuneIsDigit && !currentRuneIsDigit {
+			resultStringBuilder.WriteRune(prevRune)
+			prevRune = currentRune
+
+			isLastRune := i == length-1
+			if isLastRune {
+				resultStringBuilder.WriteRune(currentRune)
+			}
+			continue
+		}
+
+		if !prevRuneIsDigit {
+			repeatCount, _ := strconv.Atoi(string(currentRune))
+			stringForAdd := strings.Repeat(string(prevRune), repeatCount)
+			resultStringBuilder.WriteString(stringForAdd)
+			prevRune = currentRune
+			continue
+		}
+
+		prevRune = currentRune
+
+		isLastRune := i == length-1
+		if isLastRune {
+			resultStringBuilder.WriteRune(currentRune)
+		}
+	}
+
+	textUnpack := resultStringBuilder.String()
+	return textUnpack, nil
 }
