9cd83f1152c75ae2b142d4c4eca957407e50d113
Григорий Карман <g.karman@mail.ru>
2025-04-14 21:44:52 +0300

    hw08_envdir_tool добавлены простые тесты для RunCmd

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -3,5 +3,26 @@ package main
 import "testing"
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("check success code", func(t *testing.T) {
+		cmd := []string{"echo", "hello"}
+		env := Environment{}
+
+		code := RunCmd(cmd, env)
+
+		if code != 0 {
+			t.Errorf("Expected exit code 0, got %d", code)
+		}
+	})
+
+	t.Run("check error code", func(t *testing.T) {
+		cmd := []string{"sh", "-c", "exit 42"}
+		env := Environment{}
+
+		code := RunCmd(cmd, env)
+
+		if code != 42 {
+			t.Errorf("Expected exit code 42, got %d", code)
+		}
+	})
+
 }
