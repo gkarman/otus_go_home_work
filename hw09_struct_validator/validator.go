3af857e2a3f9be9f2207a4fa413e051a8ff8be37
Григорий Карман <g.karman@mail.ru>
2025-05-02 16:01:40 +0300

    hw09_struct_validator тестирование

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,24 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+var ErrMustBeStruct = errors.New("только для структуры")
+var ErrInvalidTag = errors.New("тег невалидный")
+var ErrValidationRuleDoesntSupportTypeField = errors.New("тип не поддерживается")
+var ErrValueTooSmall = errors.New("значение меньше минимального")
+var ErrValueTooBig = errors.New("значение выше максимального")
+var ErrLenString = errors.New("строка не той длинны")
+var ErrRegexpString = errors.New("значение не нужного формата")
+var ErrValueNotInList = errors.New("значение не входит в допустимый список")
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -7,11 +26,343 @@ type ValidationError struct {
 
 type ValidationErrors []ValidationError
 
-func (v ValidationErrors) Error() string {
-	panic("implement me")
+type ValidateFunc func(ValidationFieldData) (ValidationErrors, error)
+
+type ValidationFieldData struct {
+	Field     reflect.StructField
+	Value     reflect.Value
+	ruleValue string
 }
 
-func Validate(v interface{}) error {
-	// Place your code here.
+func (v ValidationErrors) Error() string {
+	if len(v) == 0 {
+		return ""
+	}
+
+	var sb strings.Builder
+	sb.WriteString("Ошибки валидации:\n")
+
+	for _, err := range v {
+		sb.WriteString(fmt.Sprintf(" - Поле '%s': %s\n", err.Field, err.Err))
+	}
+
+	return sb.String()
+}
+
+func Validate(structForValidate interface{}) error {
+	structType := reflect.TypeOf(structForValidate)
+	if structType.Kind() != reflect.Struct {
+		return ErrMustBeStruct
+	}
+
+	var allValidationErrors ValidationErrors
+	structValues := reflect.ValueOf(structForValidate)
+	for i := 0; i < structType.NumField(); i++ {
+		structField := structType.Field(i)
+		valueField := structValues.Field(i)
+
+		fieldValidationErrors, err := processValidateField(structField, valueField)
+		if err != nil {
+			return ErrInvalidTag
+		}
+		if fieldValidationErrors != nil {
+			allValidationErrors = append(allValidationErrors, fieldValidationErrors...)
+		}
+	}
+
+	if allValidationErrors == nil {
+		return nil
+	}
+
+	return allValidationErrors
+}
+
+func processValidateField(structField reflect.StructField, valueField reflect.Value) (ValidationErrors, error) {
+	if !structField.IsExported() {
+		return nil, nil
+	}
+
+	validateTag := structField.Tag.Get("validate")
+	if validateTag == "" {
+		return nil, nil
+	}
+
+	var fieldValidationErrors ValidationErrors
+
+	validateRules := strings.Split(validateTag, "|")
+	for _, validateRule := range validateRules {
+		validateRuleParts := strings.Split(validateRule, ":")
+		if len(validateRuleParts) != 2 {
+			return nil, ErrInvalidTag
+		}
+		validateRuleName := validateRuleParts[0]
+		validateRuleValue := validateRuleParts[1]
+
+		functionForValidate, err := getFunctionForValidateByRuleName(validateRuleName)
+		if err != nil {
+			return nil, fmt.Errorf("Тег валидации не поддерживается %w", err)
+		}
+
+		validationFieldData := ValidationFieldData{structField, valueField, validateRuleValue}
+		return functionForValidate(validationFieldData)
+
+	}
+	return fieldValidationErrors, nil
+
+}
+
+func getFunctionForValidateByRuleName(ruleName string) (ValidateFunc, error) {
+	validateFunctions := map[string]ValidateFunc{
+		"len":    processValidateLen,
+		"min":    processValidateMin,
+		"max":    processValidateMax,
+		"regexp": processValidateRegexp,
+		"in":     processValidateIn,
+	}
+
+	function, ok := validateFunctions[ruleName]
+	if !ok {
+		return nil, fmt.Errorf("<UNK> <UNK> <UNK> %s", ruleName)
+	}
+
+	return function, nil
+}
+
+func processValidateLen(vfd ValidationFieldData) (ValidationErrors, error) {
+	var fieldValidationErrors ValidationErrors
+
+	ruleConstraint, err := strconv.Atoi(vfd.ruleValue)
+	if err != nil {
+		return fieldValidationErrors, ErrInvalidTag
+	}
+
+	isOneValue := vfd.Value.Kind() == reflect.String
+	isSlice := vfd.Field.Type.Kind() == reflect.Slice && vfd.Field.Type.Elem().Kind() == reflect.String
+
+	if !isOneValue && !isSlice {
+		return fieldValidationErrors, ErrValidationRuleDoesntSupportTypeField
+	}
+
+	itemsForValidate := []string{}
+	if isOneValue {
+		itemsForValidate = append(itemsForValidate, vfd.Value.String())
+	}
+
+	if isSlice {
+		for i := 0; i < vfd.Value.Len(); i++ {
+			itemsForValidate = append(itemsForValidate, vfd.Value.Index(i).String())
+		}
+	}
+
+	for i := 0; i < len(itemsForValidate); i++ {
+		if err := checkLenString(itemsForValidate[i], ruleConstraint); err != nil {
+			fieldValidationErrors = append(fieldValidationErrors, ValidationError{vfd.Field.Name, err})
+		}
+	}
+
+	return fieldValidationErrors, nil
+}
+
+func processValidateMin(vfd ValidationFieldData) (ValidationErrors, error) {
+	var fieldValidationErrors ValidationErrors
+
+	ruleConstraint, err := strconv.Atoi(vfd.ruleValue)
+	if err != nil {
+		return fieldValidationErrors, ErrInvalidTag
+	}
+
+	isOneValue := vfd.Value.Kind() == reflect.Int
+	isSlice := vfd.Field.Type.Kind() == reflect.Slice && vfd.Field.Type.Elem().Kind() == reflect.Int
+
+	if !isOneValue && !isSlice {
+		return fieldValidationErrors, ErrValidationRuleDoesntSupportTypeField
+	}
+
+	itemsForValidate := []int{}
+	if isOneValue {
+		itemsForValidate = append(itemsForValidate, int(vfd.Value.Int()))
+	}
+
+	if isSlice {
+		for i := 0; i < vfd.Value.Len(); i++ {
+			itemsForValidate = append(itemsForValidate, int(vfd.Value.Index(i).Int()))
+		}
+	}
+
+	for i := 0; i < len(itemsForValidate); i++ {
+		if err := checkMinInt(itemsForValidate[i], ruleConstraint); err != nil {
+			fieldValidationErrors = append(fieldValidationErrors, ValidationError{vfd.Field.Name, err})
+		}
+	}
+
+	return fieldValidationErrors, nil
+}
+
+func processValidateMax(vfd ValidationFieldData) (ValidationErrors, error) {
+	var fieldValidationErrors ValidationErrors
+
+	ruleConstraint, err := strconv.Atoi(vfd.ruleValue)
+	if err != nil {
+		return fieldValidationErrors, ErrInvalidTag
+	}
+
+	isOneValue := vfd.Value.Kind() == reflect.Int
+	isSlice := vfd.Field.Type.Kind() == reflect.Slice && vfd.Field.Type.Elem().Kind() == reflect.Int
+
+	if !isOneValue && !isSlice {
+		return fieldValidationErrors, ErrValidationRuleDoesntSupportTypeField
+	}
+
+	itemsForValidate := []int{}
+	if isOneValue {
+		itemsForValidate = append(itemsForValidate, int(vfd.Value.Int()))
+	}
+
+	if isSlice {
+		for i := 0; i < vfd.Value.Len(); i++ {
+			itemsForValidate = append(itemsForValidate, int(vfd.Value.Index(i).Int()))
+		}
+	}
+
+	for i := 0; i < len(itemsForValidate); i++ {
+		if err := checkMaxInt(itemsForValidate[i], ruleConstraint); err != nil {
+			fieldValidationErrors = append(fieldValidationErrors, ValidationError{vfd.Field.Name, err})
+		}
+	}
+
+	return fieldValidationErrors, nil
+}
+
+func processValidateRegexp(vfd ValidationFieldData) (ValidationErrors, error) {
+	var fieldValidationErrors ValidationErrors
+
+	ruleConstraint, err := regexp.Compile(vfd.ruleValue)
+	if err != nil {
+		return fieldValidationErrors, ErrInvalidTag
+	}
+
+	isOneValue := vfd.Value.Kind() == reflect.String
+	isSlice := vfd.Field.Type.Kind() == reflect.Slice && vfd.Field.Type.Elem().Kind() == reflect.String
+
+	if !isOneValue && !isSlice {
+		return fieldValidationErrors, ErrValidationRuleDoesntSupportTypeField
+	}
+
+	itemsForValidate := []string{}
+	if isOneValue {
+		itemsForValidate = append(itemsForValidate, vfd.Value.String())
+	}
+
+	if isSlice {
+		for i := 0; i < vfd.Value.Len(); i++ {
+			itemsForValidate = append(itemsForValidate, vfd.Value.Index(i).String())
+		}
+	}
+
+	for i := 0; i < len(itemsForValidate); i++ {
+		if err := checkRegexp(itemsForValidate[i], ruleConstraint); err != nil {
+			fieldValidationErrors = append(fieldValidationErrors, ValidationError{vfd.Field.Name, err})
+		}
+	}
+
+	return fieldValidationErrors, nil
+}
+
+func processValidateIn(vfd ValidationFieldData) (ValidationErrors, error) {
+	var fieldValidationErrors ValidationErrors
+	ruleConstraints := strings.Split(vfd.ruleValue, ",")
+
+	isOneValueString := vfd.Value.Kind() == reflect.String
+	isSliceStrings := vfd.Field.Type.Kind() == reflect.Slice && vfd.Field.Type.Elem().Kind() == reflect.String
+	isOneValueInt := vfd.Value.Kind() == reflect.Int
+	isSliceInts := vfd.Field.Type.Kind() == reflect.Slice && vfd.Field.Type.Elem().Kind() == reflect.Int
+
+	if !isOneValueString && !isSliceStrings && !isOneValueInt && !isSliceInts {
+		return fieldValidationErrors, ErrValidationRuleDoesntSupportTypeField
+	}
+
+	if isOneValueString || isSliceStrings {
+		itemsForValidate := []string{}
+		if isOneValueString {
+			itemsForValidate = append(itemsForValidate, vfd.Value.String())
+		}
+
+		if isSliceStrings {
+			for i := 0; i < vfd.Value.Len(); i++ {
+				itemsForValidate = append(itemsForValidate, vfd.Value.Index(i).String())
+			}
+		}
+
+		for i := 0; i < len(itemsForValidate); i++ {
+			if err := checkIn(itemsForValidate[i], ruleConstraints); err != nil {
+				fieldValidationErrors = append(fieldValidationErrors, ValidationError{vfd.Field.Name, err})
+			}
+		}
+	}
+
+	if isOneValueInt || isSliceInts {
+		itemsForValidateInt := []int{}
+		if isOneValueInt {
+			itemsForValidateInt = append(itemsForValidateInt, int(vfd.Value.Int()))
+		}
+
+		if isSliceInts {
+			for i := 0; i < vfd.Value.Len(); i++ {
+				itemsForValidateInt = append(itemsForValidateInt, int(vfd.Value.Index(i).Int()))
+			}
+		}
+		ruleConstraintsInt := []int{}
+		for _, ruleConstraintString := range ruleConstraints {
+			ruleConstraintInt, err := strconv.Atoi(ruleConstraintString)
+			if err != nil {
+				return fieldValidationErrors, ErrInvalidTag
+			}
+			ruleConstraintsInt = append(ruleConstraintsInt, ruleConstraintInt)
+		}
+
+		for i := 0; i < len(itemsForValidateInt); i++ {
+			if err := checkIn(itemsForValidateInt[i], ruleConstraintsInt); err != nil {
+				fieldValidationErrors = append(fieldValidationErrors, ValidationError{vfd.Field.Name, err})
+			}
+		}
+	}
+
+	return fieldValidationErrors, nil
+}
+
+func checkLenString(s string, maxLen int) error {
+	if utf8.RuneCountInString(s) != maxLen {
+		return ErrLenString
+	}
 	return nil
+}
+
+func checkMinInt(v int, vMin int) error {
+	if v < vMin {
+		return ErrValueTooSmall
+	}
+	return nil
+}
+
+func checkMaxInt(v int, vMax int) error {
+	if v > vMax {
+		return ErrValueTooBig
+	}
+	return nil
+}
+
+func checkRegexp(v string, regexp *regexp.Regexp) error {
+	if !regexp.MatchString(v) {
+		return ErrRegexpString
+	}
+	return nil
+}
+
+func checkIn[T interface{ string | int }](v T, ruleConstraints []T) error {
+	for _, ruleConstraint := range ruleConstraints {
+		if v == ruleConstraint {
+			return nil
+		}
+	}
+	return ErrValueNotInList
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,6 +2,7 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -11,7 +12,7 @@ type UserRole string
 // Test the function on different structures and other types.
 type (
 	User struct {
-		ID     string `json:"id" validate:"len:36"`
+		ID     string `json:"id" validate:"len:1"`
 		Name   string
 		Age    int             `validate:"min:18|max:50"`
 		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
@@ -38,14 +39,128 @@ type (
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
-		in          interface{}
-		expectedErr error
+		structForValidate interface{}
+		expectedErr       error
 	}{
 		{
-			// Place your code here.
+			"somethingString",
+			ErrMustBeStruct,
 		},
-		// ...
-		// Place your code here.
+		{
+			User{
+				"1",
+				"Ivan",
+				18,
+				"ivan@mail.ru",
+				"admin",
+				[]string{"79111111111", "79222222222"},
+				[]byte{1},
+			},
+			nil,
+		},
+		{
+			User{
+				"qwertyuiopoiuytrewqwertyuioiuytre",
+				"Ivan",
+				18,
+				"ivan@mail.ru",
+				"admin",
+				[]string{"79111111111", "79222222222"},
+				[]byte{1},
+			},
+			ErrLenString,
+		},
+		{
+			User{
+				"1",
+				"Ivan",
+				17,
+				"ivan@mail.ru",
+				"admin",
+				[]string{"79111111111", "79222222222"},
+				[]byte{1},
+			},
+			ErrValueTooSmall,
+		},
+		{
+			User{
+				"1",
+				"Ivan",
+				90,
+				"ivan@mail.ru",
+				"admin",
+				[]string{"79111111111", "79222222222"},
+				[]byte{1},
+			},
+			ErrValueTooBig,
+		},
+		//{
+		//	User{
+		//		"1",
+		//		"Ivan",
+		//		18,
+		//		"noemail",
+		//		"admin",
+		//		[]string{"79111111111", "79222222222"},
+		//		[]byte{1},
+		//	},
+		//	ErrRegexpString,
+		//},
+		//{
+		//	User{
+		//		"1",
+		//		"Ivan",
+		//		18,
+		//		"ivan@mail.ru",
+		//		"admin11",
+		//		[]string{"79111111111", "79222222222"},
+		//		[]byte{1},
+		//	},
+		//	ErrValueNotInList,
+		//}, {
+		//	User{
+		//		"1",
+		//		"Ivan",
+		//		18,
+		//		"ivan@mail.ru",
+		//		"admin",
+		//		[]string{"79", "79"},
+		//		[]byte{1},
+		//	},
+		//	ErrLenString,
+		//}, {
+		//	App{
+		//		"12345",
+		//	},
+		//	nil,
+		//}, {
+		//	App{
+		//		"1234",
+		//	},
+		//	ErrLenString,
+		//},
+		//{
+		//	Token{
+		//		[]byte{1, 2, 3},
+		//		[]byte{1, 2, 3},
+		//		[]byte{1, 2, 3},
+		//	},
+		//	nil,
+		//},
+		//{
+		//	Response{
+		//		200,
+		//		"somestring",
+		//	},
+		//	nil,
+		//},
+		//{
+		//	Response{
+		//		211,
+		//		"somestring",
+		//	},
+		//	ErrValueNotInList,
+		//},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +168,38 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.structForValidate)
+			if tt.expectedErr == nil {
+				if err != nil {
+					t.Errorf("ожидалась ошибка <nil>, но получено: %v", err)
+				}
+				return
+			}
+
+			if errors.Is(err, tt.expectedErr) {
+				return
+			}
+
+			if tt.expectedErr == nil && err != nil {
+				t.Errorf("получили ошибку, хотя не должны были %v: ", err)
+			}
+
+			var valErrs ValidationErrors
+			if errors.As(err, &valErrs) {
+				found := false
+				for _, vErr := range valErrs {
+					if errors.Is(vErr.Err, tt.expectedErr) {
+						found = true
+						break
+					}
+				}
+				if !found {
+					t.Errorf("ожидалась ошибка %v, но в списке валидации её нет: %v", tt.expectedErr, err)
+				}
+				return
+			}
+
+			t.Errorf("ожидалась ошибка %v, но получено: %v", tt.expectedErr, err)
 		})
 	}
 }
